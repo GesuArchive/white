3269e3c8512fc280b3cc6a599d232f6f1a8d8059
Valtos <valtos@spaces.ru>
2019-10-27 16:08:58 +0700

    фикс

--- fuck.sh
+++ fuck.sh
@@ -3,7 +3,7 @@ FROM=WINDOWS-1251
 TO=UTF-8
 ICONV="iconv -f $FROM -t $TO"
 # Convert
-find . -type f -name "*.dm" | while read fn; do
+find . -type f -name "*.json" | while read fn; do
 cp ${fn} ${fn}.bak
 $ICONV < ${fn}.bak > ${fn}
 rm ${fn}.bak
