d755af0b308df9e3d46a127d48d544189b417262
baldenysh <46993894+baldenysh@users.noreply.github.com>
2020-05-21 00:58:48 +0300

    и так сойдет

--- tgui/packages/tgui/interfaces/VerbMenu.js
+++ tgui/packages/tgui/interfaces/VerbMenu.js
@@ -2,32 +2,34 @@ import { useBackend, useLocalState } from '../backend';
 import { Button, LabeledList, Tabs } from '../components';
 import { Window } from '../layouts';
 
-export const GameMenu = (props, context) => {
+export const VerbMenu = (props, context) => {
   const { act, data } = useBackend(context);
   const keys = Object.keys(data);
   const [tab, setTab] = useLocalState(context, 'tab', 0);
   return (
     <Window resizable>
       <Window.Content scrollable>
-        <Tabs>
-          {keys.map((val, key) => {
-            return (
-              <Tabs.Tab
-                key={key}
-                // icon="list"
-                lineHeight="23px"
-                selected={tab === key}
-                onClick={() => setTab(key)}>
-                {val}
-              </Tabs.Tab>);
-          })}
-        </Tabs>
+        {!(keys.length > 1) || (
+          <Tabs>
+            {keys.map((val, key) => {
+              return (
+                <Tabs.Tab
+                  key={key}
+                  // icon="list"
+                  lineHeight="25px"
+                  selected={tab === key}
+                  onClick={() => setTab(key)}>
+                  {val}
+                </Tabs.Tab>);
+            })}
+          </Tabs>
+        )}
         <LabeledList>
           {data[keys[tab]].map((val, key) => {
             return (
               <LabeledList.Item label={val[0]} key={key}>
-                <Button content={val[1].split("/").pop()} onClick={() =>
-                  act(val[1].split("/").pop())} />
+                <Button content={val[1]} onClick={() =>
+                  act(val[1])} />
               </LabeledList.Item>
             );
           })}
