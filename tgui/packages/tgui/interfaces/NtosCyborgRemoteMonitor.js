4388a0f3afa23ae4421f61c835332de6da16a2f9
zxaber <37497534+zxaber@users.noreply.github.com>
2021-03-19 15:53:45 -0700

    Adds the ability to download a borg's logs using SiliConnect. (#57300)
    About The Pull Request
    
    Adds a function to Siliconnect in which you can tap a borg using a mobile device that is currently running the software, and it will download the borg's logs. These logs list events such as being locked or unlocked, taken offline and being restored, and the process of being emagged. Borg logs will now list the emag user's name as a new user.
    
    Downloading the logs is done by right-clicking the borg while the software is open and active. This will "tap" the borg with the device, initiating the log transfer automatically. The transfer will take eight seconds to complete, requiring you and the borg to stay adjacent the entire time, and the borg will get a large red text alert about the upload when it starts. The logs are also not stored permanently on the device, and will be lost if the app is closed.
    
    This PR also introduces the tap() proc for modular computer programs, and adds functionality the the computers to call it when right-clicking with the tablet as a tool. Should the app use the tap in a meaningful way (such as starting a borg log download), it will return TRUE, and the computer will end the secondary attack chain.
    
    Currently, this requires using a tablet or laptop with Siliconnect, as you cannot tap with a console. Someday I hope to add an additional hardware option for consoles in the form of a wireless hand scanner to replicate tapping.
    Why It's Good For The Game
    
    Adds a neat way to "diagnose" a borg acting oddly, assuming you have a way to keep them still. Allows one to view if the borg has been emagged, by whom, what SiliConnect messages the borg has received, the number of law changes that have been made, as well as some other (somewhat fluff) information relating to taking damage and getting upgrades.
    Changelog
    
    🆑
    add: You can now download a cyborg's internal logs by right-clicking them with a mobile device running SiliConnect. Take a look if one is acting a bit strange, you might find something interesting.
    add: Borg integrity (health) is now roughly shown in SiliConnect, under "Condition".
    /🆑
    
    To Do:
    
    Fix the to_chat not being sent to the borg when a log transfer breaks due to distance
    Add some sort of text feedback when tapping an atom with a device, or viewing one being tapped.
    Possibly restrict syndicate borgs from having their logs viewed by SiliConnect (and likewise with station borgs and Roboverlord) Changed my mind on this, will implement later if we find it's needed.
    
        Change the log area of the window to expand naturally rather than use a fixed height
    
    image
    
    I'm not super deadset on the emag user's name being listed. It seems like a neat function unique to having a tablet running Siliconnect (as most of the other info is already available through other means), and one can always pop the brain out for a re-borg and demand the name anyway.

--- tgui/packages/tgui/interfaces/NtosCyborgRemoteMonitor.js
+++ tgui/packages/tgui/interfaces/NtosCyborgRemoteMonitor.js
@@ -1,5 +1,5 @@
-import { useBackend } from '../backend';
-import { Box, Button, LabeledList, NoticeBox, Section } from '../components';
+import { useBackend, useSharedState } from '../backend';
+import { Box, Button, LabeledList, NoticeBox, ProgressBar, Section, Stack, Tabs } from '../components';
 import { NtosWindow } from '../layouts';
 
 export const NtosCyborgRemoteMonitor = (props, context) => {
@@ -7,19 +7,39 @@ export const NtosCyborgRemoteMonitor = (props, context) => {
     <NtosWindow
       width={600}
       height={800}>
-      <NtosWindow.Content scrollable>
+      <NtosWindow.Content>
         <NtosCyborgRemoteMonitorContent />
       </NtosWindow.Content>
     </NtosWindow>
   );
 };
 
+export const ProgressSwitch = param => {
+  switch (param) {
+    case -1:
+      return '_';
+    case 0:
+      return 'Connecting';
+    case 25:
+      return 'Starting Transfer';
+    case 50:
+      return 'Downloading';
+    case 75:
+      return 'Downloading';
+    case 100:
+      return 'Formatting';
+  }
+};
+
 export const NtosCyborgRemoteMonitorContent = (props, context) => {
   const { act, data } = useBackend(context);
+  const [tab_main, setTab_main] = useSharedState(context, 'tab_main', 1);
   const {
     card,
     cyborgs = [],
+    DL_progress,
   } = data;
+  const storedlog = data.borglog || [];
 
   if (!cyborgs.length) {
     return (
@@ -30,64 +50,134 @@ export const NtosCyborgRemoteMonitorContent = (props, context) => {
   }
 
   return (
-    <>
-      {!card && (
-        <NoticeBox>
-          Для особых прав необходима ID-карта.
-        </NoticeBox>
+    <Stack fill vertical>
+      <Stack.Item>
+        <Tabs>
+          <Tabs.Tab
+            icon="robot"
+            lineHeight="23px"
+            selected={tab_main === 1}
+            onClick={() => setTab_main(1)}>
+            Киборги
+          </Tabs.Tab>
+          <Tabs.Tab
+            icon="clipboard"
+            lineHeight="23px"
+            selected={tab_main === 2}
+            onClick={() => setTab_main(2)}>
+            Журналы
+          </Tabs.Tab>
+        </Tabs>
+      </Stack.Item>
+      {tab_main === 1 && (
+        <>
+          {!card && (
+            <Stack.Item>
+              <NoticeBox>
+                Некоторый функционал требует авторизации.
+              </NoticeBox>
+            </Stack.Item>
+          )}
+          <Stack.Item grow={1}>
+            <Section fill scrollable>
+              {cyborgs.map(cyborg => (
+                <Section
+                  key={cyborg.ref}
+                  title={cyborg.name}
+                  buttons={(
+                    <Button
+                      icon="terminal"
+                      content="Отправить сообщение"
+                      color="blue"
+                      disabled={!card}
+                      onClick={() => act('messagebot', {
+                        ref: cyborg.ref,
+                      })} />
+                  )}>
+                  <LabeledList>
+                    <LabeledList.Item label="Состояние">
+                      <Box color={cyborg.status
+                        ? 'bad'
+                        : cyborg.locked_down
+                          ? 'average'
+                          : 'good'}>
+                        {cyborg.status
+                          ? "Не отвечает"
+                          : cyborg.locked_down
+                            ? "Заблокирован"
+                            : cyborg.shell_discon
+                              ? "Стабильное/Отключен"
+                              : "Стабильное"}
+                      </Box>
+                    </LabeledList.Item>
+                    <LabeledList.Item label="Целостность">
+                      <Box color={cyborg.integ <= 25
+                        ? 'bad'
+                        : cyborg.integ <= 75
+                          ? 'average'
+                          : 'good'}>
+                        {cyborg.integ === 0
+                          ? "Повреждения"
+                          : cyborg.integ <= 25
+                            ? "Серьёзные повреждения"
+                            : cyborg.integ <= 75
+                              ? "Практически уничтожен"
+                              : "Номинально"}
+                      </Box>
+                    </LabeledList.Item>
+                    <LabeledList.Item label="Заряд">
+                      <Box color={cyborg.charge <= 30
+                        ? 'bad'
+                        : cyborg.charge <= 70
+                          ? 'average'
+                          : 'good'}>
+                        {typeof cyborg.charge === 'number'
+                          ? cyborg.charge + "%"
+                          : "Не найден"}
+                      </Box>
+                    </LabeledList.Item>
+                    <LabeledList.Item label="Модель">
+                      {cyborg.module}
+                    </LabeledList.Item>
+                    <LabeledList.Item label="Обновления">
+                      {cyborg.upgrades}
+                    </LabeledList.Item>
+                  </LabeledList>
+                </Section>
+              ))}
+            </Section>
+          </Stack.Item>
+        </>
       )}
-      {cyborgs.map(cyborg => {
-        return (
-          <Section
-            key={cyborg.ref}
-            title={cyborg.name}
-            buttons={(
-              <Button
-                icon="terminal"
-                content="Отправить сообщение"
-                color="blue"
-                disabled={!card}
-                onClick={() => act('messagebot', {
-                  ref: cyborg.ref,
-                })} />
-            )}>
-            <LabeledList>
-              <LabeledList.Item label="Состояние">
-                <Box color={cyborg.status
-                  ? 'bad'
-                  : cyborg.locked_down
-                    ? 'average'
-                    : 'good'}>
-                  {cyborg.status
-                    ? "Не отвечает"
-                    : cyborg.locked_down
-                      ? "Заблокирован"
-                      : cyborg.shell_discon
-                        ? "Стабильное/Отключен"
-                        : "Стабильное"}
+      {tab_main === 2 && (
+        <>
+          <Stack.Item>
+            <Section>
+              Отсканируй киборга чтобы скачать журнал.
+              <ProgressBar
+                value={DL_progress/100}>
+                {ProgressSwitch(DL_progress)}
+              </ProgressBar>
+            </Section>
+          </Stack.Item>
+          <Stack.Item
+            grow={1}>
+            <Section
+              fill
+              scrollable
+              backgroundColor="black">
+              {storedlog.map(log => (
+                <Box
+                  mb={1}
+                  key={log}
+                  color="green">
+                  {log}
                 </Box>
-              </LabeledList.Item>
-              <LabeledList.Item label="Заряд">
-                <Box color={cyborg.charge <= 30
-                  ? 'bad'
-                  : cyborg.charge <= 70
-                    ? 'average'
-                    : 'good'}>
-                  {typeof cyborg.charge === 'number'
-                    ? cyborg.charge + "%"
-                    : "Не найдено"}
-                </Box>
-              </LabeledList.Item>
-              <LabeledList.Item label="Модуль">
-                {cyborg.module}
-              </LabeledList.Item>
-              <LabeledList.Item label="Улучшения">
-                {cyborg.upgrades}
-              </LabeledList.Item>
-            </LabeledList>
-          </Section>
-        );
-      })}
-    </>
+              ))}
+            </Section>
+          </Stack.Item>
+        </>
+      )}
+    </Stack>
   );
 };
