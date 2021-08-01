HolyTigerGod/Voidcrew

Circuit submodules (#60109)
Adds the module component that is basically a subroutine. Allows you to compact your logic into a bunch of functions.

code/modules/wiremod/integrated_circuit.dm
@@ -53,7 +53,7 @@
 
 /obj/item/integrated_circuit/loaded/Initialize()
 	. = ..()
-	cell = new /obj/item/stock_parts/cell/high(src)
+	set_cell(new /obj/item/stock_parts/cell/high(src))
 
 /obj/item/integrated_circuit/Destroy()
 	for(var/obj/item/circuit_component/to_delete in attached_components)
@@ -73,6 +73,10 @@
 	else
 		. += span_notice("There is no power cell installed.")
 
+/obj/item/integrated_circuit/proc/set_cell(obj/item/stock_parts/cell_to_set)
+	SEND_SIGNAL(src, COMSIG_CIRCUIT_SET_CELL, cell_to_set)
+	cell = cell_to_set
+
 /obj/item/integrated_circuit/attackby(obj/item/I, mob/living/user, params)
 	. = ..()
 	if(istype(I, /obj/item/circuit_component))
@@ -85,7 +89,7 @@
 			return
 		if(!user.transferItemToLoc(I, src))
 			return
-		cell = I
+		set_cell(I)
 		I.add_fingerprint(user)
 		user.visible_message(span_notice("[user] inserts a power cell into [src]."), span_notice("You insert the power cell into [src]."))
 		return
@@ -101,7 +105,7 @@
 		I.play_tool_sound(src)
 		user.visible_message(span_notice("[user] unscrews the power cell from [src]."), span_notice("You unscrew the power cell from [src]."))
 		cell.forceMove(drop_location())
-		cell = null
+		set_cell(null)
 		return
 
 /**
@@ -114,7 +118,8 @@
  */
 /obj/item/integrated_circuit/proc/set_shell(atom/movable/new_shell)
 	remove_current_shell()
-	on = TRUE
+	set_on(TRUE)
+	SEND_SIGNAL(src, COMSIG_CIRCUIT_SET_SHELL, new_shell)
 	shell = new_shell
 	RegisterSignal(shell, COMSIG_PARENT_QDELETING, .proc/remove_current_shell)
 	for(var/obj/item/circuit_component/attached_component as anything in attached_components)
@@ -122,8 +127,6 @@
 		// Their input ports may be updated with user values, but the outputs haven't updated
 		// because on is FALSE
 		TRIGGER_CIRCUIT_COMPONENT(attached_component, null)
-	if(display_name != "")
-		shell.name = "[initial(shell.name)] ([display_name])"
 
 /**
  * Unregisters the current shell attached to this circuit.
@@ -137,8 +140,12 @@
 		attached_component.unregister_shell(shell)
 	UnregisterSignal(shell, COMSIG_PARENT_QDELETING)
 	shell = null
-	on = FALSE
+	set_on(FALSE)
 	SEND_SIGNAL(src, COMSIG_CIRCUIT_SHELL_REMOVED)
+
+/obj/item/integrated_circuit/proc/set_on(new_value)
+	SEND_SIGNAL(src, COMSIG_CIRCUIT_SET_ON, new_value)
+	on = new_value
 
 /**
  * Adds a component to the circuitboard
@@ -428,9 +435,9 @@
 			var/new_name = params["display_name"]
 
 			if(new_name)
-				display_name = strip_html(params["display_name"], label_max_length)
+				set_display_name(strip_html(params["display_name"], label_max_length))
 			else
-				display_name = ""
+				set_display_name("")
 
 			if(shell)
 				if(display_name != "")
@@ -457,6 +464,10 @@
 	return COMSIG_CANCEL_USB_CABLE_ATTACK
 
 #undef WITHIN_RANGE
+
+/// Sets the display name that appears on the shell.
+/obj/item/integrated_circuit/proc/set_display_name(new_name)
+	display_name = new_name
 
 /**
  * Returns the creator of the integrated circuit. Used in admin messages and other related things.

GesuArchive/white

code/modules/wiremod/integrated_circuit.dm
@@ -50,7 +50,7 @@
 
 /obj/item/integrated_circuit_wiremod/loaded/Initialize()
 	. = ..()
-	cell = new /obj/item/stock_parts/cell/high(src)
+	set_cell(new /obj/item/stock_parts/cell/high(src))
 
 /obj/item/integrated_circuit_wiremod/Destroy()
 	for(var/obj/item/circuit_component/to_delete in attached_components)
@@ -71,6 +71,10 @@
 	else
 		. += "<span class='notice'>Внутри нет батарейки.</span>"
 
+/obj/item/integrated_circuit_wiremod/proc/set_cell(obj/item/stock_parts/cell_to_set)
+	SEND_SIGNAL(src, COMSIG_CIRCUIT_SET_CELL, cell_to_set)
+	cell = cell_to_set
+
 /obj/item/integrated_circuit_wiremod/attackby(obj/item/I, mob/living/user, params)
 	. = ..()
 	if(istype(I, /obj/item/circuit_component))
@@ -83,7 +87,7 @@
 			return
 		if(!user.transferItemToLoc(I, src))
 			return
-		cell = I
+		set_cell(I)
 		I.add_fingerprint(user)
 		user.visible_message("<span class='notice'>[user] вставляет батарейку в [src.name].</span>", "<span class='notice'>Устанавливаю батарейку в [src.name].</span>")
 		return
@@ -99,7 +103,7 @@
 		I.play_tool_sound(src)
 		user.visible_message("<span class='notice'>[user] вытаскивает батарейку из [src.name].</span>", "<span class='notice'>Выкручиваю батарейку из [src.name].</span>")
 		cell.forceMove(drop_location())
-		cell = null
+		set_cell(null)
 		return
 
 /**
@@ -112,7 +116,8 @@
  */
 /obj/item/integrated_circuit_wiremod/proc/set_shell(atom/movable/new_shell)
 	remove_current_shell()
-	on = TRUE
+	set_on(TRUE)
+	SEND_SIGNAL(src, COMSIG_CIRCUIT_SET_SHELL, new_shell)
 	shell = new_shell
 	RegisterSignal(shell, COMSIG_PARENT_QDELETING, .proc/remove_current_shell)
 	for(var/obj/item/circuit_component/attached_component as anything in attached_components)
@@ -120,8 +125,6 @@
 		// Their input ports may be updated with user values, but the outputs haven't updated
 		// because on is FALSE
 		TRIGGER_CIRCUIT_COMPONENT(attached_component, null)
-	if(display_name != "")
-		shell.name = "[initial(shell.name)] ([display_name])"
 
 /**
  * Unregisters the current shell attached to this circuit.
@@ -135,8 +138,12 @@
 		attached_component.unregister_shell(shell)
 	UnregisterSignal(shell, COMSIG_PARENT_QDELETING)
 	shell = null
-	on = FALSE
+	set_on(FALSE)
 	SEND_SIGNAL(src, COMSIG_CIRCUIT_SHELL_REMOVED)
+
+/obj/item/integrated_circuit/proc/set_on(new_value)
+	SEND_SIGNAL(src, COMSIG_CIRCUIT_SET_ON, new_value)
+	on = new_value
 
 /**
  * Adds a component to the circuitboard
@@ -421,9 +428,9 @@
 			var/new_name = params["display_name"]
 
 			if(new_name)
-				display_name = strip_html(params["display_name"], label_max_length)
+				set_display_name(strip_html(params["display_name"], label_max_length))
 			else
-				display_name = ""
+				set_display_name("")
 
 			if(shell)
 				if(display_name != "")
@@ -449,6 +456,10 @@
 	return COMSIG_CANCEL_USB_CABLE_ATTACK
 
 #undef WITHIN_RANGE
+
+/// Sets the display name that appears on the shell.
+/obj/item/integrated_circuit/proc/set_display_name(new_name)
+	display_name = new_name
 
 /**
  * Returns the creator of the integrated circuit. Used in admin messages and other related things.