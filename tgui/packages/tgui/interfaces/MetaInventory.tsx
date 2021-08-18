eef3360b19b1c7c40b0d593b85decd438428d8cc
baldenysh <46993894+baldenysh@users.noreply.github.com>
2021-08-18 20:55:06 +0300

    Update MetaInventory.tsx

--- tgui/packages/tgui/interfaces/MetaInventory.tsx
+++ tgui/packages/tgui/interfaces/MetaInventory.tsx
@@ -5,16 +5,15 @@ import { useBackend } from "../backend";
 import { Box, Button, Icon, Stack } from "../components";
 import { Window } from "../layouts";
 
-const ROWS = 5;
+const ROWS = 6;
 const COLUMNS = 8;
 
 const BUTTON_DIMENSIONS = "50px";
 
 type GridSpotKey = string;
 
-
 //getGridSpotKey, переименовал потомушто смешной линт 80 символов на строку
-const getGridSpotKey = (spot: [number, number]): GridSpotKey => {
+const getGSK = (spot: [number, number]): GridSpotKey => {
   return `${spot[0]}/${spot[1]}`;
 };
 
@@ -47,131 +46,34 @@ const SLOTS: Record<
     additionalComponent?: JSX.Element;
   }
 > = {
-  "8": {
-    displayName: "глаза",
-    gridSpot: getGridSpotKey([0, 0]),
-    image: "inventory-glasses.png",
-  },
-
-  "64": {
-    displayName: "голова",
-    gridSpot: getGridSpotKey([0, 1]),
-    image: "inventory-head.png",
-  },
-
-  "4096": {
-    displayName: "шея",
-    gridSpot: getGridSpotKey([1, 0]),
-    image: "inventory-neck.png",
-  },
-
-  "32": {
-    displayName: "маска",
-    gridSpot: getGridSpotKey([1, 1]),
-    image: "inventory-mask.png",
-  },
-
-  "16": {
-    displayName: "уши",
-    gridSpot: getGridSpotKey([1, 2]),
-    image: "inventory-ears.png",
-  },
-
-  "2": {
-    displayName: "униформа",
-    gridSpot: getGridSpotKey([2, 0]),
-    image: "inventory-uniform.png",
-  },
-
-  "1": {
-    displayName: "костюм",
-    gridSpot: getGridSpotKey([2, 1]),
-    image: "inventory-suit.png",
-  },
-
-  "4": {
-    displayName: "перчатки",
-    gridSpot: getGridSpotKey([2, 2]),
-    image: "inventory-gloves.png",
-  },
-
-  "128": {
-    displayName: "ботинки",
-    gridSpot: getGridSpotKey([3, 1]),
-    image: "inventory-shoes.png",
-  },
-
-  "32768": {
-    displayName: "хранилище костюма",
-    gridSpot: getGridSpotKey([3, 2]),
-    image: "inventory-suit_storage.png",
-  },
-
-  "256": {
-    displayName: "ID",
-    gridSpot: getGridSpotKey([4, 0]),
-    image: "inventory-id.png",
-  },
-
-  "512": {
-    displayName: "пояс",
-    gridSpot: getGridSpotKey([4, 1]),
-    image: "inventory-belt.png",
-  },
-
-  "1024": {
-    displayName: "спина",
-    gridSpot: getGridSpotKey([4, 2]),
-    image: "inventory-back.png",
-  },
-
-  "65536": {
-    displayName: "левый карман",
-    gridSpot: getGridSpotKey([4, 3]),
-    image: "inventory-pocket.png",
-  },
-
-  "131072": {
-    displayName: "правый карман",
-    gridSpot: getGridSpotKey([4, 4]),
-    image: "inventory-pocket.png",
-  },
-
-  "-1": {
-    displayName: "левая рука",
-    gridSpot: getGridSpotKey([2, 4]),
-    image: "inventory-hand_l.png",
-    additionalComponent: <CornerText align="right">Л</CornerText>,
-  },
-
-  "-2": {
-    displayName: "правая рука",
-    gridSpot: getGridSpotKey([2, 3]),
-    image: "inventory-hand_r.png",
-    additionalComponent: <CornerText align="left">П</CornerText>,
-  },
-
+  "8":{displayName:"глаза",gridSpot:getGSK([0, 0]),image:"glasses.png",},
+  "64":{displayName:"голова",gridSpot:getGSK([0, 1]),image:"head.png",},
+  "4096":{displayName:"шея",gridSpot:getGSK([1, 0]),image:"neck.png",},
+  "32":{displayName:"маска",gridSpot:getGSK([1, 1]),image:"mask.png",},
+  "16":{displayName:"уши",gridSpot:getGSK([1, 2]),image:"ears.png",},
+  "2":{displayName:"униформа",gridSpot:getGSK([2, 0]),image:"uniform.png",},
+  "1":{displayName:"костюм",gridSpot: getGSK([2, 1]),image:"suit.png",},
+  "4":{displayName:"перчатки",gridSpot:getGSK([2, 2]),image:"gloves.png",},
+  "128":{displayName:"ботинки",gridSpot:getGSK([3, 1]),image:"shoes.png",},
+  "32768":{displayName:"хранилище костюма",gridSpot:getGSK([3, 2]),
+    image: "suit_storage.png",},
+  "256":{displayName:"ID",gridSpot:getGSK([4, 0]),image:"id.png",},
+  "512":{displayName:"пояс",gridSpot:getGSK([4, 1]),image:"belt.png",},
+  "1024":{displayName:"спина",gridSpot: getGSK([4, 2]),image:"back.png",},
+  "65536":{displayName:"левый карман",gridSpot:getGSK([4, 3]),
+    image: "pocket.png",},
+  "131072":{displayName:"правый карман",gridSpot:getGSK([4, 4]),
+    image: "pocket.png",},
+  "-1":{displayName:"левая рука",gridSpot:getGSK([2, 4]),image:"hand_l.png",
+    additionalComponent: <CornerText align="right">Л</CornerText>,},
+  "-2":{displayName:"правая рука",gridSpot:getGSK([2, 3]),image:"hand_r.png",
+    additionalComponent: <CornerText align="left">П</CornerText>,},
   //аыыыыыыыыыыыы я дыбиииииииил
-  "-3": {displayName: "пол", gridSpot: getGridSpotKey([0, 6]), image: "inventory-pocket.png",},
-  "-4": {displayName: "пол", gridSpot: getGridSpotKey([1, 6]), image: "inventory-pocket.png",},
-  "-5": {displayName: "пол", gridSpot: getGridSpotKey([2, 6]), image: "inventory-pocket.png",},
-  "-6": {displayName: "пол", gridSpot: getGridSpotKey([3, 6]), image: "inventory-pocket.png",},
-  "-7": {displayName: "пол", gridSpot: getGridSpotKey([4, 6]), image: "inventory-pocket.png",},
-  //"-8": {displayName: "пол6", gridSpot: getGridSpotKey([5, 6]), image: "inventory-pocket.png",},
-
-/*
-  "-1": {
-    displayName: "курсор",
-    gridSpot: getGridSpotKey([4, 7]),
-    image: "inventory-pocket.png",
-  },
-
-  "0": {
-    displayName: "инвентарь",
-    gridSpot: getGridSpotKey([4, 8]),
-    image: "inventory-pocket.png",
-  },
-*/
+  "-3":{displayName: "пол", gridSpot: getGSK([0, 6]), image: "pocket.png",},
+  "-4":{displayName: "пол", gridSpot: getGSK([1, 6]), image: "pocket.png",},
+  "-5":{displayName: "пол", gridSpot: getGSK([2, 6]), image: "pocket.png",},
+  "-6":{displayName: "пол", gridSpot: getGSK([3, 6]), image: "pocket.png",},
+  "-7":{displayName: "пол", gridSpot: getGSK([4, 6]), image: "pocket.png",},
 };
 
 type MetaInvObj = {
@@ -205,7 +107,7 @@ export const MetaInventory = (props, context) => {
             <Stack.Item key={row}>
               <Stack fill>
                 {range(0, COLUMNS).map(column => {
-                  const key = getGridSpotKey([row, column]);
+                  const key = getGSK([row, column]);
                   const keyAtSpot = gridSpots.get(key);
 
                   if (!keyAtSpot) {
@@ -283,7 +185,7 @@ export const MetaInventory = (props, context) => {
                           {slot.image && (
                             <Box
                               as="img"
-                              src={resolveAsset(slot.image)}
+                              src={resolveAsset("inventory-" + slot.image)}
                               opacity={0.7}
                               style={{
                                 position: "absolute",
