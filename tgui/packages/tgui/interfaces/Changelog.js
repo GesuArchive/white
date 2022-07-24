82d30f2d501eb70b6a2118e7d82aec75650d7115
Valtos <valtos@spaces.ru>
2022-07-25 00:28:30 +0700

    aw shit

--- tgui/packages/tgui/interfaces/Changelog.js
+++ tgui/packages/tgui/interfaces/Changelog.js
@@ -1,45 +1,62 @@
 import { useBackend } from '../backend';
 import { Icon, Section } from '../components';
 import { Window } from '../layouts';
+import { map, uniqBy } from 'common/collections';
+import { flow } from 'common/fp';
 
 const changelogIcons = {
   "+": {
+    desc: "Добавлено",
     type: "plus",
   },
   "-": {
+    desc: "Удалено",
     type: "minus",
   },
   "=": {
+    desc: "Исправлено",
     type: "wrench",
   },
   "a": {
+    desc: "Сломано",
     type: "cross",
   },
 };
 
 export const Changelog = (props, context) => {
   const { act, data } = useBackend(context);
-  const {
-    all_changelog = [],
-  } = data;
+  const { all_changelog } = data;
   return (
     <Window
       title="Список изменений"
+      theme="hackerman"
       width={400}
       height={700}>
       <Window.Content scrollable>
-        {all_changelog.reverse().map(entry => (
-          <Section
-            title={new Date(parseInt(entry.timestamp, 10) * 1000)
-              .toLocaleDateString("ru-RU") + " - " + entry.author}
-            key={entry.timestamp}>
-            <Icon
-              name={changelogIcons[entry.content.substring(0, 1)].type}
-              rotation={0}
-              spin={0} />
-            {entry.content.substring(1)}
-          </Section>
-        ))}
+        {all_changelog.reverse().map(entry => {
+          let splitted_clog = entry.content.split("\n");
+          return (
+            <Section
+              title={new Date(parseInt(entry.timestamp, 10) * 1000)
+                .toLocaleDateString("ru-RU") + " - " + entry.author}
+              key={entry.timestamp}>
+              {splitted_clog.map(es => {
+                let clog_icon = es.substring(0, 1) in changelogIcons
+                  ? changelogIcons[es.substring(0, 1)].type
+                  : null;
+                return (
+                  <div key={es}>
+                    <Icon
+                      name={clog_icon ? clog_icon : "cross"}
+                      rotation={0}
+                      spin={0} />
+                    {clog_icon ? es.substring(1) : es}
+                  </div>
+                );
+              })}
+            </Section>
+          );
+        })}
       </Window.Content>
     </Window>
   );
