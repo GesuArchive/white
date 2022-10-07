18fd8197fc5b8033ee1bfc38bc18989d075c141d
Valtos <valtos@spaces.ru>
2022-10-07 21:22:05 +0700

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -5,10 +5,10 @@
 
 # byond version
 export BYOND_MAJOR=514
-export BYOND_MINOR=1568
+export BYOND_MINOR=1575
 
 #rust_g git tag
-export RUST_G_VERSION=0.4.7
+export RUST_G_VERSION=1.0.2
 
 #node version
 export NODE_VERSION=14
@@ -27,4 +27,4 @@ export PYTHON_VERSION=3.7.9
 export AUXLUA_REPO=tgstation/auxlua
 
 #auxlua git tag
-export AUXLUA_VERSION=1.1.1
+export AUXLUA_VERSION=1.3.0
